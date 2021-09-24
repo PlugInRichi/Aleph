5ca3e5eee06483d59ed9bfc09fe05bb01d64ae8f
Ricardo García G <48848372+PlugInRichi@users.noreply.github.com>
2021-09-23 23:11:15 -0500

    Delete config files directory

--- config files/refresh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Actualizando archivos..."
-
-cp ~/.bashrc bashrc
-pacman -Qet > pakagesPacman.txt
-cp ~/.config/qtile/config.py QT-config.py
-cp ~/.config/alacritty/alacritty.yml alacritty.yml
-echo "hecho"
-
